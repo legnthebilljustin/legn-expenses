59ada3fca2eea0b48f6c0d25ed7155bd58b94fbe
legnthebilljustin <124340867+legnthebilljustin@users.noreply.github.com>
2025-01-02 07:56:56 +0000

    fixed failed login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,5 @@
 import { auth } from "@/firebase/config";
 import { setUid } from "@/state/authSlice";
-import { openErrorModal, setErrorDetails } from "@/state/errorSlice";
 import { Button, Input } from "@nextui-org/react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
@@ -11,6 +10,8 @@ import { useAuthRedirect } from "@/hooks/useAuthRedirect";
 export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false)
+    const [error, setError] = useState<null | string>(null)
 
     useAuthRedirect()
 
@@ -18,6 +19,8 @@ export default function Login() {
     const dispatch = useDispatch()
 
     const handleLogin = async() => {
+        setError(null)
+        setIsFormSubmitted(true)
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password)
             const user = userCredential.user
@@ -25,8 +28,10 @@ export default function Login() {
             dispatch(setUid(user.uid))
             navigate('/dashboard', { replace: true })
         } catch(error) {
-            dispatch(setErrorDetails({ message: "Invalid credentials.", code: 401 }))
-            dispatch(openErrorModal())
+            // not dispatching error reducer here since this component lives outside of the App component tree
+            setError("Invalid credentials.")
+        } finally {
+            setIsFormSubmitted(false)
         }
     }
     
@@ -34,23 +39,38 @@ export default function Login() {
         <div className="min-h-screen flex items-center justify-center dark">
             <div className="w-full h-full max-w-[400px] max-h-[600px] text-center">
                 <p className="mb-6 text-lg">Sign In</p>
+                { (!isFormSubmitted && error) &&
+                    <div className="error-alert">
+                        Invalid credentials!
+                    </div>
+                }
+                    
                 <Input size="sm" isRequired
                     name="email" label="Email" className="mb-2"
                     endContent={<i className='bx bxl-mailchimp bx-sm'></i>}
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    isDisabled={isFormSubmitted}
                 />
                 <Input size="sm" isRequired type="password"
                     name="password" label="Password" className="mb-2"
                     endContent={<i className='bx bxs-lock-alt bx-sm' ></i>}
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    isDisabled={isFormSubmitted}
                 />
                 
                 <Button size="md" variant="flat"
                     className="w-full mt-3"
                     onClick={handleLogin}
-                >Sign in to your Account</Button>
+                    isDisabled={isFormSubmitted}
+                >
+                    { !isFormSubmitted ? "Sign in to your Account" :
+                        <>
+                            Logging you in. Please wait <i className='bx bx-loader-circle bx-spin' />
+                        </>
+                    }
+                </Button>
                 <p className="mt-4 text-sm text-blue-300 cursor-pointer">Forgot your password?</p>
             </div>
         </div>
--- src/state/errorSlice.ts
+++ src/state/errorSlice.ts
@@ -30,7 +30,6 @@ const errorSlice = createSlice({
             state.code = undefined
         },
         setErrorDetails: (state, action: PayloadAction<ErrorPayloadType>) => {
-            console.log('setting error details')
             const { message, code } = action.payload
 
             if (!message || !code) {
